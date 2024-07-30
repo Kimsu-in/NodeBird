11efb675b575d5e0de3f5987d88c2e4c04473de7
Kimsu-in <tndls0258@naver.com>
2024-07-30 15:22:06 +0900

    이미지 캐루셀 react-slick 적용

--- prepare/front/components/PostImages.js
+++ prepare/front/components/PostImages.js
@@ -1,6 +1,7 @@
 import { PlusOutlined } from "@ant-design/icons";
 import PropTypes from "prop-types";
 import { useCallback, useState } from "react";
+import ImagesZoom from "./imagesZoom";
 
 const PostImages = ({ images }) => {
   const [showImageZoom, setShowImagesZoom] = useState(false);
@@ -8,6 +9,11 @@ const PostImages = ({ images }) => {
   const onZoom = useCallback(() => {
     setShowImagesZoom(true);
   }, []);
+
+  const onClose = useCallback(() => {
+    setShowImagesZoom(false);
+  }, []);
+
   if (images.length === 1) {
     return (
       <>
@@ -17,6 +23,7 @@ const PostImages = ({ images }) => {
           alt={images[0].src}
           onClick={onZoom}
         />
+        {showImageZoom && <ImagesZoom images={images} onClose={onClose} />}
       </>
     );
   }
@@ -37,33 +44,37 @@ const PostImages = ({ images }) => {
           alt={images[1].src}
           onClick={onZoom}
         />
+        {showImageZoom && <ImagesZoom images={images} onClose={onClose} />}
       </>
     );
   }
   return (
-    <div>
-      <img
-        role="presentation"
-        style={{ width: "50%" }}
-        src={images[0].src}
-        alt={images[0].src}
-        onClick={onZoom}
-      />
-      <div
-        role="presentation"
-        style={{
-          display: "inline-block",
-          width: "50%",
-          textAlign: "center",
-          verticalAlign: "middle",
-        }}
-        onClick={onZoom}
-      >
-        <PlusOutlined />
-        <br />
-        {images.length - 1}개의 사진 더보기
+    <>
+      <div>
+        <img
+          role="presentation"
+          style={{ width: "50%" }}
+          src={images[0].src}
+          alt={images[0].src}
+          onClick={onZoom}
+        />
+        <div
+          role="presentation"
+          style={{
+            display: "inline-block",
+            width: "50%",
+            textAlign: "center",
+            verticalAlign: "middle",
+          }}
+          onClick={onZoom}
+        >
+          <PlusOutlined />
+          <br />
+          {images.length - 1}개의 사진 더보기
+        </div>
       </div>
-    </div>
+      {showImageZoom && <ImagesZoom images={images} onClose={onClose} />}
+    </>
   );
 };
 
