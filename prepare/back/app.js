9326d628950d10d91f1cce7293343b52c2c9632c
Kimsu-in <tndls0258@naver.com>
2024-08-14 10:17:37 +0900

    express 적용 및 라우터

--- prepare/back/app.js
+++ prepare/back/app.js
@@ -1,10 +1,23 @@
-const http = require("http");
-const server = http.createServer((req, res) => {
-  console.log(req.url, req.method);
-  res.write("hello node");
-  res.end("hello node");
+const express = require("express");
+const postRouter = require("./routes/post");
+const app = express();
+
+app.get("/", (req, res) => {
+  // 브라우저 주소창은 get요청
+  res.send("hello express");
 });
 
-server.listen(3065, () => {
+app.get("/api/posts", (req, res) => {
+  res.json([
+    // 응답은 json
+    { id: 1, content: "hello1" },
+    { id: 2, content: "hello2" },
+    { id: 3, content: "hello3" },
+  ]);
+});
+
+app.use("/post", postRouter);
+
+app.listen(3065, () => {
   console.log("서버 실행 중...");
 });
